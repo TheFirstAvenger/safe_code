36a621b2ccdea0295a6671bf05e3296166a880f3
TheFirstAvenger <mike@mikebinns.net>
2023-03-08 15:55:58 -0500

    Add Github Actions CI

--- mix.exs
+++ mix.exs
@@ -10,7 +10,12 @@ defmodule SafeCode.MixProject do
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/TheFirstAvenger/safe_code",
       deps: deps(),
-      package: package()
+      package: package(),
+      dialyzer: [
+        ignore_warnings: ".dialyzer_ignore.exs",
+        list_unused_filters: true,
+        plt_file: {:no_warn, "plts/safe_code.plt"}
+      ]
     ]
   end
 
@@ -35,6 +40,7 @@ defmodule SafeCode.MixProject do
       {:phoenix_live_view, "~> 0.18.3"},
       {:jason, "~> 1.3"},
       {:credo, "~> 1.6.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
