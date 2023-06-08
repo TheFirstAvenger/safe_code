9907b385f1b71c0602183bf7fd8732570252b622
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2023-06-08 18:11:27 -0300

    Support TagEngine from PhoenixLiveView 0.18.17 (#8)
    Co-authored-by: Mike Binns <TheFirstAvenger@users.noreply.github.com>

--- lib/safe_code/heex_parser.ex
+++ lib/safe_code/heex_parser.ex
@@ -1,14 +1,28 @@
 defmodule SafeCode.HeexParser do
   def parse_template(expr) do
-    options = [
-      engine: Phoenix.LiveView.TagEngine,
-      file: "foo",
-      line: 1,
-      caller: __ENV__,
-      source: expr,
-      trim: true,
-      tag_handler: Phoenix.LiveView.HTMLEngine
-    ]
+    options =
+      if Code.ensure_loaded?(Phoenix.LiveView.TagEngine) do
+        [
+          engine: Phoenix.LiveView.TagEngine,
+          file: "foo",
+          line: 1,
+          caller: __ENV__,
+          source: expr,
+          trim: true,
+          tag_handler: Phoenix.LiveView.HTMLEngine
+        ]
+      else
+        [
+          engine: Phoenix.LiveView.HTMLEngine,
+          file: "foo",
+          line: 1,
+          module: MyModule,
+          indentation: 0,
+          caller: __ENV__,
+          source: expr,
+          trim: true
+        ]
+      end
 
     EEx.compile_string(expr, options)
   end
