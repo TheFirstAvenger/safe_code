3b951e67dabedba3360760ad95db2951d856fd33
Leandro Pereira <leandro@leandro.io>
2024-03-04 11:43:25 -0500

    Relax deps version requirements
    To avoid conflict on new projects

--- mix.exs
+++ mix.exs
@@ -19,7 +19,6 @@ defmodule SafeCode.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
@@ -33,17 +32,14 @@ defmodule SafeCode.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.1.0", env: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.1", env: :dev, runtime: false},
       {:phoenix_live_view, ">= 0.18.17"},
       {:jason, "~> 1.3"},
-      {:credo, "~> 1.6.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
